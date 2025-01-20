013ef9eb11f18b2e4c55aca7a75a13941a87cb61
KalashT <kalashthakare898@gmail.com>
2025-01-20 20:58:57 +0530

    payment verification

--- backend/controlers/paymentControler.js
+++ backend/controlers/paymentControler.js
@@ -1,5 +1,6 @@
 
 import { instance } from "../server.js"
+import crypto from "crypto"
 
 export const checkout = async (req,res)=>{
     const options={
@@ -15,6 +16,14 @@ export const checkout = async (req,res)=>{
     });
 }
 
-export const paymentVerification=()=>{
-    
+export const paymentVerification=async(req,res)=>{
+
+    const {razorpay_order_id,razorpay_payment_id,razorpay_signature}=req.body;
+
+    const body=razorpay_order_id + "|" + razorpay_payment_id
+
+    const expectedSignature= crypto.createHmac('sha256',process.env.RAZORPAY_SECRET).update(body.toString()).digest('hex');
+
+    console.log(razorpay_signature);
+    console.log(expectedSignature);
 }
